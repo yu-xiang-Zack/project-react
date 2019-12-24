f786ddb5cae115f657872e623e02b189553113c3
xiangyu960408@163.com <xiangyu960408@163.com>
2019-12-24 22:35:27 +0800

    *

---
+++ frontend/src/AddDesk.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import api from './api'
+import history from './history'
+
+function AddDesk() {
+  var [deskInfo, setDeskInfo] = useState({
+    name: '',
+    capacity: '',
+  })
+
+  function change(e) {
+    setDeskInfo({
+      ...deskInfo,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  
+  function submit(e) {
+    e.preventDefault()
+    api.post('/restaurant/:rid/desk', deskInfo).then(res => {
+      history.goBack()
+    })
+  }
+  
+
+  return (
+    <div>
+      <h2>添加菜品</h2>
+      <form onSubmit={submit}>
+        名称：<input type="text" onChange={change} defaultValue={deskInfo.name} name="name"/><br/>
+        数量：<input type="text" onChange={change} defaultValue={deskInfo.capacity} name="capacity"/><br/>
+        <button>提交</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddDesk
